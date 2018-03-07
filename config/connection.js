2124ad684c2db05fb2dc1b7e586994976668872d
Kevin Hinkel <kevinhinkel@Kevin-Hinkels-iMac.local>
2018-03-07 14:19:18 -0600

    creation of JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,8 @@
+// Set up database connection
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'burgers_db'
-});
+var connection;
 
+// add in the environment variable option for JAWSDB for heroku
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
@@ -14,12 +11,22 @@ if (process.env.JAWSDB_URL) {
     user: 'root',
     password: '',
     database: 'burgers_db'
-	})
-}
+	});
+};
 
+
+// var connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'burgers_db'
+// });
+
+// not strictly required, but best practice to establish connection
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id: " + connection.threadId);
 });
 
+// export the connection back to orm
 module.exports = connection;
