5b84de8c4cf1db0faab677bde61811bd1662850c
Kevin Hinkel <kevinhinkel@Kevin-Hinkels-iMac.local>
2018-03-07 14:00:44 -0600

    connection change

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,17 @@ var connection = mysql.createConnection({
     database: 'burgers_db'
 });
 
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+	host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'burgers_db'
+	})
+}
+
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id: " + connection.threadId);
